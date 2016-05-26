43ebc456bedc9acdeb44a3f52ecf5c4d2c793af8
Elec332 <arnout1998@gmail.com>
2016-05-26 16:43:14 +0200

    Connector Stuff

--- src/main/java/mcjty/xnet/multiblock/XNetGrid.java
+++ src/main/java/mcjty/xnet/multiblock/XNetGrid.java
@@ -1,13 +1,21 @@
 package mcjty.xnet.multiblock;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import mcjty.xnet.XNet;
+import mcjty.xnet.api.XNetAPI;
 import mcmultipart.multipart.IMultipart;
+import mcmultipart.multipart.IMultipartContainer;
+import mcmultipart.multipart.PartSlot;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 import javax.annotation.Nonnull;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -19,12 +27,12 @@ public class XNetGrid {
         this.worldGridRegistry = registry;
         allLocations = Sets.newHashSet();
         allLocations_ = Collections.unmodifiableSet(allLocations);
-        allConnectors = Sets.newHashSet();
+        allConnectors = Lists.newArrayList();
     }
 
     private final Set<BlockPos> allLocations;
     private final Set<BlockPos> allLocations_;
-    private final Set<FacedPosition> allConnectors;
+    private final List<FacedPosition> allConnectors;
     private final XNetWorldGridRegistry worldGridRegistry;
 
     public void tick(){
@@ -56,6 +64,7 @@ public class XNetGrid {
             }
             addTile(worldGridRegistry.getPowerTile(pos));
         }
+        allConnectors.addAll(grid.allConnectors);
     }
 
     protected void addTile(XNetTileData tile){
@@ -66,22 +75,48 @@ public class XNetGrid {
     protected void onRemoved(XNetTileData tile) {
     }
 
-    void change(XNetTileData tile, IMultipart multipart){
+    void change(XNetTileData tile, IMultipart multipart_){
+        TileEntity tileEntity = tile.getTile();
+        BlockPos pos = tile.getPos();
+        FacedPosition faces = null;
+        for (FacedPosition position : allConnectors){
+            if (position.pos.equals(pos)){
+                faces = position;
+            }
+        }
+        if (faces == null){
+            faces = new FacedPosition(pos);
+            allConnectors.add(faces);
+        }
+        faces.sides.clear();
+        if (tileEntity instanceof IMultipartContainer){
+            for (EnumFacing facing : EnumFacing.VALUES) {
+                IMultipart multipart = ((IMultipartContainer) tileEntity).getPartInSlot(PartSlot.getFaceSlot(facing));
+                if (multipart instanceof ICapabilityProvider && ((ICapabilityProvider) multipart).hasCapability(XNetAPI.XNET_CAPABILITY, facing.getOpposite())) {
+                    faces.sides.add(facing);
+                }
+            }
+        }
     }
 
     private class FacedPosition {
 
-        public FacedPosition(@Nonnull  BlockPos pos, @Nonnull EnumFacing side){
+        private FacedPosition(@Nonnull BlockPos pos){
             this.pos = pos;
-            this.side = side;
+            this.sides = EnumSet.noneOf(EnumFacing.class);
         }
 
         private final BlockPos pos;
-        private final EnumFacing side;
+        private final Set<EnumFacing> sides;
 
         @Override
         public boolean equals(Object obj) {
-            return obj instanceof FacedPosition && pos.equals(((FacedPosition) obj).pos) && side == ((FacedPosition) obj).side;
+            return obj instanceof FacedPosition && pos.equals(((FacedPosition) obj).pos) && sides.equals(((FacedPosition) obj).sides);
+        }
+
+        @Override
+        public String toString() {
+            return "Pos: "+pos+" Sides: "+sides;
         }
 
     }
