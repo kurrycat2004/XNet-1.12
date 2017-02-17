cb57994eeab3d82b8a2d128cdc4a3f30b64aad75
McJty <mcjty1@gmail.com>
2017-02-17 09:01:35 +0100

    Recipes

--- src/main/java/mcjty/xnet/init/ModRecipes.java
+++ src/main/java/mcjty/xnet/init/ModRecipes.java
@@ -1,11 +1,30 @@
 package mcjty.xnet.init;
 
 import mcjty.xnet.blocks.cables.NetCableSetup;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModRecipes {
 
     public static void init(){
         NetCableSetup.initCrafting();
+
+        Block machineFrame;
+        if (Loader.isModLoaded("rftools")) {
+            machineFrame = ForgeRegistries.BLOCKS.getValue(new ResourceLocation("rftools", "machine_frame"));
+        } else {
+            machineFrame = Blocks.IRON_BLOCK;
+        }
+
+        GameRegistry.addRecipe(new ItemStack(ModBlocks.controllerBlock), "RCR", "rMr", "igi",
+                'M', machineFrame, 'R', Items.REPEATER, 'C', Items.COMPARATOR, 'r', Items.REDSTONE,
+                'i', Items.IRON_INGOT, 'g', Items.GOLD_INGOT);
     }
 
 }
