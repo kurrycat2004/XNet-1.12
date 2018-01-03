1553ed12fde16b7fc417ebaf258ba5f657fe6c7b
McJty <mcjty1@gmail.com>
2018-01-03 07:01:16 +0100

    1.6.4

--- src/main/java/mcjty/xnet/XNet.java
+++ src/main/java/mcjty/xnet/XNet.java
@@ -35,7 +35,7 @@ public class XNet implements ModBase {
 
     public static final String MODID = "xnet";
     public static final String MODNAME = "XNet";
-    public static final String MODVERSION = "1.6.3";
+    public static final String MODVERSION = "1.6.4";
 
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_MCJTYLIB_VER = "2.6.2";
