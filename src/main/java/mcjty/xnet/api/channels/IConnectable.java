0328d7428863bf9658623c4cf0808286083256ee
McJty <mcjty1@gmail.com>
2018-01-05 11:23:18 +0100

    Typo

--- src/main/java/mcjty/xnet/api/channels/IConnectable.java
+++ src/main/java/mcjty/xnet/api/channels/IConnectable.java
@@ -20,11 +20,11 @@ public interface IConnectable {
      * @param connectorPos the position of the connector (not your block!)
      * @param blockPos the position of your block
      * @param tileEntity a tile entity at this block position. Can be null if there is no tile entity here
-     * @param facting the direction (as seen from the connector) towards your block
+     * @param facing the direction (as seen from the connector) towards your block
      */
     ConnectResult canConnect(@Nonnull IBlockAccess access, @Nonnull BlockPos connectorPos, @Nonnull BlockPos blockPos,
                              @Nullable TileEntity tileEntity,
-                             @Nonnull EnumFacing facting);
+                             @Nonnull EnumFacing facing);
 
     enum ConnectResult {
         NO,                 // No connection possible. Don't try further
