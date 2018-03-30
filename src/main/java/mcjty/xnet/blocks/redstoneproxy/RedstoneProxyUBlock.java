64c0b96d086ae439985739848b4d74784bf933fc
Joseph C. Sible <josephcsible@users.noreply.github.com>
2018-03-29 21:20:37 -0400

    Add missing import

--- src/main/java/mcjty/xnet/blocks/redstoneproxy/RedstoneProxyUBlock.java
+++ src/main/java/mcjty/xnet/blocks/redstoneproxy/RedstoneProxyUBlock.java
@@ -11,6 +11,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 
