f364688638d82f0df6678c22e4e511298ac00ac4
McJty <mcjty1@gmail.com>
2018-05-14 11:09:08 +0200

    Rationalized the proxies. There is now an AbstractCommonProxy in McJtyLib

--- src/main/java/mcjty/xnet/proxy/CommonProxy.java
+++ src/main/java/mcjty/xnet/proxy/CommonProxy.java
@@ -1,9 +1,8 @@
 package mcjty.xnet.proxy;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import mcjty.lib.McJtyLib;
 import mcjty.lib.base.GeneralConfig;
 import mcjty.lib.network.PacketHandler;
+import mcjty.lib.proxy.AbstractCommonProxy;
 import mcjty.lib.varia.WrenchChecker;
 import mcjty.xnet.CommandHandler;
 import mcjty.xnet.ForgeEventHandlers;
@@ -17,8 +16,6 @@ import mcjty.xnet.gui.GuiProxy;
 import mcjty.xnet.init.ModBlocks;
 import mcjty.xnet.init.ModItems;
 import mcjty.xnet.network.XNetMessages;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.FMLLog;
@@ -30,21 +27,17 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import org.apache.logging.log4j.Level;
 
 import java.io.File;
-import java.util.concurrent.Callable;
 
-public abstract class CommonProxy {
+public abstract class CommonProxy extends AbstractCommonProxy {
 
-    public static File modConfigDir;
-    private Configuration mainConfig;
-
+    @Override
     public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
         MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers());
-        McJtyLib.preInit(e);
         CommandHandler.registerCommands();
 
         GeneralConfig.preInit(e);
 
-        modConfigDir = e.getModConfigurationDirectory();
         mainConfig = new Configuration(new File(modConfigDir.getPath() + File.separator + "xnet", "xnet.cfg"));
 
         readMainConfig();
@@ -77,29 +70,16 @@ public abstract class CommonProxy {
         }
     }
 
+    @Override
     public void init(FMLInitializationEvent e) {
+        super.init(e);
         NetworkRegistry.INSTANCE.registerGuiHandler(XNet.instance, new GuiProxy());
     }
 
+    @Override
     public void postInit(FMLPostInitializationEvent e) {
+        super.postInit(e);
         mainConfig = null;
         WrenchChecker.init();
     }
-
-    public World getClientWorld() {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public EntityPlayer getClientPlayer() {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public <V> ListenableFuture<V> addScheduledTaskClient(Callable<V> callableToSchedule) {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
 }
