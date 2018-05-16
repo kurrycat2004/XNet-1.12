c47b24b8e03830abbe271ae16d058d90d7fffd1d
McJty <mcjty1@gmail.com>
2018-05-16 09:44:59 +0200

    Removed the deprecated GenericBlock constructors

--- src/main/java/mcjty/xnet/blocks/generic/GenericXNetBlock.java
+++ src/main/java/mcjty/xnet/blocks/generic/GenericXNetBlock.java
@@ -1,33 +1,27 @@
 package mcjty.xnet.blocks.generic;
 
 import mcjty.lib.blocks.GenericBlock;
-import mcjty.lib.blocks.GenericItemBlock;
 import mcjty.lib.tileentity.GenericTileEntity;
 import mcjty.xnet.XNet;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.function.BiFunction;
+
 public abstract class GenericXNetBlock<T extends GenericTileEntity, C extends Container> extends GenericBlock<T, C> {
 
-    public GenericXNetBlock(Material material,
-                            Class<? extends T> tileEntityClass,
-                            Class<? extends C> containerClass,
-                            String name, boolean isContainer) {
-        this(material, tileEntityClass, containerClass, GenericItemBlock.class, name, isContainer);
-    }
-
-    public GenericXNetBlock(Material material,
-                            Class<? extends T> tileEntityClass,
-                            Class<? extends C> containerClass,
-                            Class<? extends ItemBlock> itemBlockClass,
-                            String name, boolean isContainer) {
-        super(XNet.instance, material, tileEntityClass, containerClass, name, isContainer);
+    public GenericXNetBlock(Material material, Class<? extends T> tileEntityClass,
+                            BiFunction<EntityPlayer, IInventory, C> containerFactory,
+                            Class<? extends ItemBlock> itemBlockClass, String name, boolean isContainer) {
+        super(XNet.instance, material, tileEntityClass, containerFactory, itemBlockClass, name, isContainer);
         setCreativeTab(XNet.tabXNet);
     }
 
