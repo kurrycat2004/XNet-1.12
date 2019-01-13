39a790e587efb6dbc20177f3c3eff66bfe2e8f58
McJty <mcjty1@gmail.com>
2019-01-13 09:50:14 +0100

    Version 1.7.6

--- src/main/java/mcjty/xnet/XNet.java
+++ src/main/java/mcjty/xnet/XNet.java
@@ -38,10 +38,10 @@ public class XNet implements ModBase {
 
     public static final String MODID = "xnet";
     public static final String MODNAME = "XNet";
-    public static final String MODVERSION = "1.7.6_beta2";
+    public static final String MODVERSION = "1.7.6";
 
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
-    public static final String MIN_MCJTYLIB_VER = "3.0.5";
+    public static final String MIN_MCJTYLIB_VER = "3.1.0";
     public static final String MIN_RFTOOLS_VER = "7.50";
 
     public static final String SHIFT_MESSAGE = "<Press Shift>";
