6ba9eeaf4a75f64537874179534e10331b2b6bd3
McJty <mcjty1@gmail.com>
2019-07-22 05:13:45 +0200

    XNet bumped to 1.8.2

--- src/main/java/mcjty/xnet/XNet.java
+++ src/main/java/mcjty/xnet/XNet.java
@@ -31,7 +31,7 @@ public class XNet implements ModBase {
 
     public static final String MODID = "xnet";
     public static final String MODNAME = "XNet";
-    public static final String MODVERSION = "1.8.1";
+    public static final String MODVERSION = "1.8.2";
 
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
     public static final String MIN_MCJTYLIB_VER = "3.5.0";
